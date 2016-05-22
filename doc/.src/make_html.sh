3cff3894d6a92364d5ce0b30f1656f7a67327f69
Hans Petter Langtangen <hpl@simula.no>
2016-05-22 02:05:48 -0700

    updates

--- doc/.src/make_html.sh
+++ doc/.src/make_html.sh
@@ -25,18 +25,18 @@ opt="CHAPTER=$CHAPTER BOOK=$BOOK APPENDIX=$APPENDIX --exercise_numbering=chapter
 # Compile standard sphinx
 # (lots of mathjax rendering failures, especially with \bar\boldsymbol{},
 # so we drop sphinx)
-theme=cbc
-theme=classic
-system doconce format sphinx $name $opt EXV=True --without_solutions --without_answers $encoding --replace_ref_by_latex_auxno=book.aux --DSPHINX_SC2 --toc_depth=2 # --sphinx_keep_splits
+themes="uio2 cbc bootstrap scipy_lectures"
+for theme in $themes; do
+system doconce format sphinx $name $opt EXV=True --without_solutions --without_answers $encoding --replace_ref_by_latex_auxno=book.aux --toc_depth=2 # --sphinx_keep_splits
 system doconce split_rst $name
 system doconce sphinx_dir theme=$theme dirname=sphinx-${theme} $name
 system python automake_sphinx.py
-exit
+done
 
 hash=82dee82e1274a586571086dca04d00308d3a0d86  # "book with solutions"
 # Compile Bootstrap HTML with solutions
 html=.trash${hash}
-system doconce format html $name $opt --html_style=bootswatch_simula --html_admon=bootstrap_panel --html_code_style=inherit "--html_body_style=font-size:20px;line-height:1.5" --html_output=$html $encoding --replace_ref_by_latex_auxno=book.aux EXV=True #--without_solutions --without_answers
+system doconce format html $name $opt --html_style=bootstrap_simula --html_admon=bootstrap_panel --html_code_style=inherit "--html_body_style=font-size:20px;line-height:1.5" --html_output=$html $encoding --replace_ref_by_latex_auxno=book.aux EXV=True #--without_solutions --without_answers
 cp $html.html tmp.html
 system doconce split_html $html.html
 #cp password.html ${topicname}-book-sol.html
@@ -47,6 +47,16 @@ cp $html.html ${topicname}-book-sol.html
 # Compile Bootstrap HTML in Simula style without answers
 html=${topicname}-book
 system doconce format html $name $opt --html_style=bootstrap_simula --html_admon=bootstrap_panel --html_code_style=inherit "--html_body_style=font-size:20px;line-height:1.5" --html_output=$html EXV=True --without_solutions --without_answers $encoding --replace_ref_by_latex_auxno=book.aux
+system doconce split_html $html.html
+
+# Compile Bootstrap HTML in readable style
+html=${topicname}-book2
+system doconce format html $name $opt --html_style=bootswatch_readable --html_admon=bootstrap_panel --html_code_style=inherit "--html_body_style=font-size:20px;line-height:1.5" --html_output=$html EXV=True $encoding --replace_ref_by_latex_auxno=book.aux
+system doconce split_html $html.html
+
+# Compile Bootstrap HTML in bluegray style
+html=${topicname}-book3
+system doconce format html $name $opt --html_style=bootstrap_bluegray --html_admon=bootstrap_panel --html_code_style=inherit "--html_body_style=font-size:20px;line-height:1.5" --html_output=$html EXV=True $encoding --replace_ref_by_latex_auxno=book.aux
 system doconce split_html $html.html
 
 # Compile solarized HTML
@@ -69,8 +79,12 @@ for figdir in $figdirs; do
         cp -r $figdir/ $dest/html
     fi
 done
+# A single sphinx theme
 #rm -rf ${dest}/sphinx
 #cp -r sphinx-${theme}/_build/html ${dest}/sphinx
+rm -f $dest/sphinx-*
+for dir in sphinx-*; do
+ cp -r $dir/_build/html ${dest}/$dir
 
 cd $dest
 git add .
